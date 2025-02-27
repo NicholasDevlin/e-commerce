1d5aa738d6c99350478f7ef7bf855cb243684197
nicholas <devlinnich@gmail.com>
2025-02-27 22:00:22 +0700

    Feat: middleware

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,10 +16,13 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        DB::table('users')->insert([
+            [
+                'name'=> 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('12345678'),
+                'is_admin' => true
+            ]
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,34 +5,42 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Redirect;
+use App\Http\Middleware\Admin;
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return Redirect::route('product');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware([Admin::class])->group(function () {
+    Route::get('/product/create', [ProductController::class, 'create'])->name('create_product');
+    Route::post('/product/create', [ProductController::class, 'store'])->name('store_product');
+    Route::get('/product/{product}/edit', [ProductController::class, 'edit'])->name('edit_product');
+    Route::patch('/product/{product}/edit', [ProductController::class, 'update'])->name('update_product');
+    Route::delete('/product/{product}', [ProductController::class, 'delete'])->name('delete_product');
+    Route::post('/order/confirm/{order}', [OrderController::class, 'confirm_payment'])->name('confirm_payment');
+});
 
 //product
 Route::get('/product', [ProductController::class, 'index'])->name('product');
-Route::get('/product/create', [ProductController::class, 'create'])->name('create_product');
-Route::post('/product/create', [ProductController::class, 'store'])->name('store_product');
-Route::get('/product/{product}', [ProductController::class, 'show'])->name('show_product');
-Route::get('/product/{product}/edit', [ProductController::class, 'edit'])->name('edit_product');
-Route::patch('/product/{product}/edit', [ProductController::class, 'update'])->name('update_product');
-Route::delete('/product/{product}', [ProductController::class, 'delete'])->name('delete_product');
-//cart
-Route::post('/cart/{product}', [CartController::class, 'create'])->name('add_to_cart');
-Route::get('/cart', [CartController::class, 'show'])->name('show_cart');
-Route::patch('/cart/{cart}', [CartController::class, 'update'])->name('update_cart');
-Route::delete('cart/{cart}', [CartController::class, 'delete'])->name('delete_cart');
-//order
-Route::get('/order', [OrderController::class, 'index'])->name('order');
-Route::get('/order/{order}', [OrderController::class, 'detail'])->name('order_detail');
-Route::post('/checkout', [OrderController::class, 'checkout'])->name('checkout');
-Route::post('/order/pay/{order}', [OrderController::class, 'submit_payment_receipt'])->name('submit_payment_receipt');
-Route::post('/order/confirm/{order}', [OrderController::class, 'confirm_payment'])->name('confirm_payment');
-//profile
-Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
-Route::post('/profile', [ProfileController::class, 'update'])->name('update_profile');
+
+Route::middleware(['auth'])->group(function () {
+    //product
+    Route::get('/product/{product}', [ProductController::class, 'show'])->name('show_product');
+    //cart
+    Route::post('/cart/{product}', [CartController::class, 'create'])->name('add_to_cart');
+    Route::get('/cart', [CartController::class, 'show'])->name('show_cart');
+    Route::patch('/cart/{cart}', [CartController::class, 'update'])->name('update_cart');
+    Route::delete('cart/{cart}', [CartController::class, 'delete'])->name('delete_cart');
+    //order
+    Route::get('/order', [OrderController::class, 'index'])->name('order');
+    Route::get('/order/{order}', [OrderController::class, 'detail'])->name('order_detail');
+    Route::post('/checkout', [OrderController::class, 'checkout'])->name('checkout');
+    Route::post('/order/pay/{order}', [OrderController::class, 'submit_payment_receipt'])->name('submit_payment_receipt');
+    //profile
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
+    Route::post('/profile', [ProfileController::class, 'update'])->name('update_profile');
+});
